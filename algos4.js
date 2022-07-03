0baa43b9e1d4c7a6f7c229ba0e7d2e20a7c2dc1b
Fanfan durand <durand90@live.com>
2022-07-02 22:53:06 -0400

    first

---
+++ algos4.js
@@ -0,0 +1,20 @@
+
+
+/* given two strings "s" and "t", return true if t is an anagra, of s,
+and false otherwise */
+/* an anagram is a word or phrase formed by rearranging the letters of a different word or phrase
+typically using the original letters exctly once */
+
+var s = "anagram"
+var t = "nagrams"
+
+var isAnagram = function(s, t) {
+    if (s.length != t.length)
+    return false;
+
+    var small = s.split('');
+    var tall = t.split('');
+    else {
+
+    }
+}
